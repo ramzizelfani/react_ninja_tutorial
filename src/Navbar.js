ba3dd3e6b43286f4e12fcaf471c99f1645c144d8
Ramzi Zelfani <ramzi.zelfani@gmail.com>
2021-01-28 15:56:59 +0100

    Initial commit3

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <h1> Laloun blog</h1>
+      <div className='links'>
+        <a href='/'>Home</a>
+        <a href='/create'>New Post</a>
+      </div>
+    </nav>
+  );
+}
